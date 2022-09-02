51967b92fd9771964654ca098be50987e8120133
zq <2285053637@qq.com>
2022-09-02 11:07:51 +0800

    format three files

--- plate_with_a_preexisting_crack_v50.m
+++ plate_with_a_preexisting_crack_v50.m
@@ -17,7 +17,7 @@ dx = length / ndivx;
 % dx: Spacing between material points
 nbnd = 3;
 % nbnd: Number of divisions in the boundary region
-totnode = ndivx * (ndivy + 2*nbnd);
+totnode = ndivx * (ndivy + 2 * nbnd);
 % totnode: Total number of material points
 
 coord = zeros(totnode, 2);
@@ -30,8 +30,8 @@ nnum = 0;
 for i = 1:ndivy
     for j = 1:ndivx
         nnum = nnum + 1;
-        coord(nnum, 1) = (-1.0*length/2.0) + (dx/2.0) + (j - 1)*dx;
-        coord(nnum, 2) = (-1.0*width/2.0) + (dx/2.0) + (i - 1)*dx;
+        coord(nnum, 1) = (-1.0 * length / 2.0) + (dx / 2.0) + (j - 1) * dx;
+        coord(nnum, 2) = (-1.0 * width / 2.0) + (dx / 2.0) + (i - 1) * dx;
     end
 end
 
@@ -39,27 +39,26 @@ totint = nnum;
 
 % Material points of the boundary region - bottom
 for i = 1:nbnd
-    for j = 1: ndivx
+    for j = 1:ndivx
         nnum = nnum + 1;
-        coord(nnum, 1) = -1.0/2.0*length + (dx/2.0) + (j - 1)*dx;
-        coord(nnum, 2) = -1.0/2.0*width - (dx/2.0) - (i - 1)*dx;
+        coord(nnum, 1) = -1.0 / 2.0 * length + (dx / 2.0) + (j - 1) * dx;
+        coord(nnum, 2) = -1.0 / 2.0 * width - (dx / 2.0) - (i - 1) * dx;
     end
 end
 
 totbottom = nnum;
 
 % Material points of the boundary region - top
-for i = 1: nbnd
-    for j = 1: ndivx
+for i = 1:nbnd
+    for j = 1:ndivx
         nnum = nnum + 1;
-        coord(nnum, 1) = -1.0/2.0*length + (dx/2.0) + (j - 1)*dx;
-        coord(nnum, 2) = 1.0/2.0*width + (dx/2.0) + (i - 1)*dx;
+        coord(nnum, 1) = -1.0 / 2.0 * length + (dx / 2.0) + (j - 1) * dx;
+        coord(nnum, 2) = 1.0 / 2.0 * width + (dx / 2.0) + (i - 1) * dx;
     end
 end
 
 tottop = nnum;
 totnode = nnum;
-
 
 %% Initialization of fail flag array
 maxfam = 100;
@@ -101,19 +100,19 @@ crlength = 0.01;
 % crlength: Crack length
 
 % PD bonds penetrating through the crack surface are broken
-for i = 1: totnode
-    for j = 1: numfam(i, 1)
-        cnode = nodefam(pointfam(i, 1) + j - 1, 1);
+for i = 1:totnode
+    for j = 1:numfam(i, 1)
+        cnode = nodefam(pointfam(i, 1)+j-1, 1);
         if ((coord(cnode, 2) > 0.0) && (coord(i, 2) < 0.0))
-            if ((abs(coord(i, 1)) - (crlength/2.0)) <= 1.0e-10)
+            if ((abs(coord(i, 1)) - (crlength / 2.0)) <= 1.0e-10)
                 fail(i, j) = 0;
-            elseif ((abs(coord(cnode, 1)) - (crlength/2.0)) <= 1.0e-10)
+            elseif ((abs(coord(cnode, 1)) - (crlength / 2.0)) <= 1.0e-10)
                 fail(i, j) = 0;
             end
         elseif ((coord(i, 2) > 0.0) && (coord(cnode, 2) < 0.0))
-            if ((abs(coord(i, 1)) - (crlength/2.0)) <= 1.0e-10)
+            if ((abs(coord(i, 1)) - (crlength / 2.0)) <= 1.0e-10)
                 fail(i, j) = 0;
-            elseif ((abs(coord(cnode, 1)) - (crlength/2.0)) <= 1.0e-10)
+            elseif ((abs(coord(cnode, 1)) - (crlength / 2.0)) <= 1.0e-10)
                 fail(i, j) = 0;
             end
         end
@@ -217,12 +216,12 @@ vel = zeros(totnode, 2);
 disp = zeros(totnode, 2);
 
 %% Stable mass vector computation
-dt = 0.8*sqrt(2.0*dens*dx/(pi*delta^2*dx*bc));
+dt = 0.8 * sqrt(2.0*dens*dx/(pi * delta^2 * dx * bc));
 % dt: Time interval
 
 % massvec = zeros(totnode, 2);
 % % massvec: massvector for adaptive dynamic relaxation
-% 
+%
 % for i = 1:totnode
 %     % 5 is a safety factor
 %     massvec(i, 1) = 0.25 * dt * dt * (pi * (delta)^2 * thick) * bc / dx;%  * 5.0;
@@ -235,7 +234,6 @@ appres = 200.0e6;
 
 bforce = zeros(totnode, 2);
 % bforce: body load acting on a material point
-
 
 %% Time integration
 nt = 1250;
@@ -251,9 +249,9 @@ scr0 = 0.04472;
 
 dmg = zeros(totint, 1);
 maxdmg = 0.5;
-maxxc = crlength/2.0;
+maxxc = crlength / 2.0;
 
-crack_length = zeros(nt,2);
+crack_length = zeros(nt, 2);
 
 pforce = zeros(totnode, 2);
 % pforce: total peridynamic force acting on a material point
@@ -281,14 +279,14 @@ for tt = 1:nt
     ctime = tt * dt;
 
     % Application of boundary conditions at the top and bottom edges
-    for i = (totint + 1): totbottom
+    for i = (totint + 1):totbottom
         vel(i, 2) = -50.0;
-        disp(i, 2) = -50.0*tt*dt;
+        disp(i, 2) = -50.0 * tt * dt;
     end
 
-    for i = (totbottom + 1): tottop
+    for i = (totbottom + 1):tottop
         vel(i, 2) = 50.0;
-        disp(i, 2) = 50.0*tt*dt;
+        disp(i, 2) = 50.0 * tt * dt;
     end
 
 
@@ -327,81 +325,81 @@ for tt = 1:nt
             scr = sqrt(scr);
 
             if (fail(i, j) == 1)
-                dforce1 = bc*(nlength - idist)/idist*vol*scr*fac*(coord(cnode, 1) + disp(cnode, 1) - coord(i, 1) - disp(i, 1))/nlength;
-                dforce2 = bc*(nlength - idist)/idist*vol*scr*fac*(coord(cnode, 2) + disp(cnode, 2) - coord(i, 2) - disp(i, 2))/nlength;
+                dforce1 = bc * (nlength - idist) / idist * vol * scr * fac * (coord(cnode, 1) + disp(cnode, 1) - coord(i, 1) - disp(i, 1)) / nlength;
+                dforce2 = bc * (nlength - idist) / idist * vol * scr * fac * (coord(cnode, 2) + disp(cnode, 2) - coord(i, 2) - disp(i, 2)) / nlength;
             else
-                    dforce1 = 0.0;
-                    dforce2 = 0.0;
+                dforce1 = 0.0;
+                dforce2 = 0.0;
             end
             pforce(i, 1) = pforce(i, 1) + dforce1;
             pforce(i, 2) = pforce(i, 2) + dforce2;
 
             % Definition of a no-fail zone
             if (abs((nlength - idist)/idist) > scr0)
-                if (abs(coord(i, 2)) <= (length/4.0))
+                if (abs(coord(i, 2)) <= (length / 4.0))
                     fail(i, j) = 0;
                 end
             end
 
-            dmgpar1 = dmgpar1 + fail(i, j)*vol*fac;
-            dmgpar2 = dmgpar2 + vol*fac;
+            dmgpar1 = dmgpar1 + fail(i, j) * vol * fac;
+            dmgpar2 = dmgpar2 + vol * fac;
         end
-        % Calculation of the damage parameter 
-        dmg(i, 1) = 1.0 - dmgpar1/dmgpar2;
+        % Calculation of the damage parameter
+        dmg(i, 1) = 1.0 - dmgpar1 / dmgpar2;
         if ((dmg(i, 1) > maxdmg) && (coord(i, 1) > maxxc))
             maxxc = coord(i, 1);
         end
     end
 
-    newcrlength = maxxc - crlength/2.0;
+    newcrlength = maxxc - crlength / 2.0;
 
-    for i = 1: totint
+    for i = 1:totint
         % Calculation of acceleration of material point i
-        acc(i, 1) = (pforce(i, 1) + bforce(i, 1))/dens;
-        acc(i, 2) = (pforce(i, 2) + bforce(i, 2))/dens;
+        acc(i, 1) = (pforce(i, 1) + bforce(i, 1)) / dens;
+        acc(i, 2) = (pforce(i, 2) + bforce(i, 2)) / dens;
         % Calculation of velocity of material point i
         % by integrating the acceleration of material point i
-        vel(i, 1) = vel(i, 1) + acc(i, 1)*dt;
-        vel(i, 2) = vel(i, 2) + acc(i, 2)*dt;
+        vel(i, 1) = vel(i, 1) + acc(i, 1) * dt;
+        vel(i, 2) = vel(i, 2) + acc(i, 2) * dt;
         % Calculation of displacement of material point i
         % by integrating the velocity of material point i
-        disp(i, 1) = disp(i, 1) + vel(i, 1)*dt;
-        disp(i, 2) = disp(i, 2) + vel(i, 2)*dt;
+        disp(i, 1) = disp(i, 1) + vel(i, 1) * dt;
+        disp(i, 2) = disp(i, 2) + vel(i, 2) * dt;
     end
 
-    for i = (totint + 1): totbottom
-        acc(i, 1) = (pforce(i, 1) + bforce(i, 1))/dens;
-        vel(i, 1) = vel(i, 1) + acc(i, 1)*dt;
-        disp(i, 1) = disp(i, 1) + vel(i, 1)*dt;
+    for i = (totint + 1):totbottom
+        acc(i, 1) = (pforce(i, 1) + bforce(i, 1)) / dens;
+        vel(i, 1) = vel(i, 1) + acc(i, 1) * dt;
+        disp(i, 1) = disp(i, 1) + vel(i, 1) * dt;
     end
 
-    for i = (totbottom + 1): tottop
-        acc(i, 1) = (pforce(i, 1) + bforce(i, 1))/dens;
-        vel(i, 1) = vel(i, 1) + acc(i, 1)*dt;
-        disp(i, 1) = disp(i, 1) + vel(i, 1)*dt;
+    for i = (totbottom + 1):tottop
+        acc(i, 1) = (pforce(i, 1) + bforce(i, 1)) / dens;
+        vel(i, 1) = vel(i, 1) + acc(i, 1) * dt;
+        disp(i, 1) = disp(i, 1) + vel(i, 1) * dt;
     end
 
     if (tt == 750)
         for i = 1:totint
-           coord_disp_pd_750_pwc(i, 1:5) = [coord(i, 1), coord(i, 2), disp(i, 1), disp(i, 2), dmg(i,1)];
+            coord_disp_pd_750_pwc(i, 1:5) = [coord(i, 1), coord(i, 2), disp(i, 1), disp(i, 2), dmg(i, 1)];
         end
     elseif (tt == 1000)
         for i = 1:totint
-           coord_disp_pd_1000_pwc(i, 1:5) = [coord(i, 1), coord(i, 2), disp(i, 1), disp(i, 2), dmg(i,1)];
+            coord_disp_pd_1000_pwc(i, 1:5) = [coord(i, 1), coord(i, 2), disp(i, 1), disp(i, 2), dmg(i, 1)];
         end
     elseif (tt == 1250)
         for i = 1:totint
-           coord_disp_pd_1250_pwc(i, 1:5) = [coord(i, 1), coord(i, 2), disp(i, 1), disp(i, 2), dmg(i,1)];
+            coord_disp_pd_1250_pwc(i, 1:5) = [coord(i, 1), coord(i, 2), disp(i, 1), disp(i, 2), dmg(i, 1)];
         end
     end
 
-    crack_length(tt,1:2) = [ctime, newcrlength];
+    crack_length(tt, 1:2) = [ctime, newcrlength];
 end
 
 %% plot
 colormap jet;
 subplot(221)
-plot(crack_length(:,1), crack_length(:,2))
+plot(crack_length(:, 1), crack_length(:, 2))
 scale = 1;
 subplot(222)
 scatter(coord_disp_pd_750_pwc(:, 1)+scale*coord_disp_pd_750_pwc(:, 3), coord_disp_pd_750_pwc(:, 2)+scale*coord_disp_pd_750_pwc(:, 4), [], coord_disp_pd_750_pwc(:, 5), "filled")
